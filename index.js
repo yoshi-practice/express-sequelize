8cfcab89fdf71b64b99da3122174d24ae4562159
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-24 14:16:06 +0900

    😍(env): express index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-var http = require('http');
+var express = require('express');
+var app = express();
 
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(1234);
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
