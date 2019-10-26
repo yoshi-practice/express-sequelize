a42ad92bd6beeaaa7e51429457a8abc94e4fd1a7
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-10-27 08:32:36 +0900

    feat(coding): sqlを綺麗に

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,4 +1,7 @@
-create database NodeTest;
+DROP DATABASE IF EXISTS NodeTest;
+CREATE DATABASE IF NOT EXISTS NodeTest;
 use NodeTest;
-create table test_table(id int(11),name varchar(255));
+create table test_table(
+    id int(11),name varchar(255)
+);
 insert into test_table values(1,'testname');
